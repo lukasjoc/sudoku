d2cb40f040ae16d04874189d9d042b2171f5d98b
lukasjoc <jochamlu@gmail.com>
2020-08-04 20:23:03 +0200

    update data to use the correct starter pattern

--- sudoku-app/src/data.ts
+++ sudoku-app/src/data.ts
@@ -1,48 +1,48 @@
 const defaultdata: object = [
   [
-    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
-    { "value": 2, "isEven": false }, { "value": 4, "isEven": false }, { "value": 2, "isEven": false },
-    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 3, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": "", "isEven": false }, { "value": "", "isEven": true },
+    { "value": "", "isEven": false }, { "value": "", "isEven": true }, { "value": "", "isEven": true },
+    { "value": "", "isEven": false }, { "value": "", "isEven": true }, { "value": 3, "isEven": false },
   ],
   [
-    { "value": "", "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
-    { "value": "", "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
-    { "value": "", "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": "", "isEven": false }, { "value": "", "isEven": false }, { "value": "", "isEven": true },
+    { "value": "", "isEven": false }, { "value": 6, "isEven": true }, { "value": "", "isEven": true },
+    { "value": "", "isEven": false }, { "value": "", "isEven": false }, { "value": "", "isEven": true },
   ],
   [
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
-    { "value": 1, "isEven": true }, { "value": 1, "isEven": true }, { "value": 1, "isEven": false },
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false }
+    { "value": "", "isEven": true }, { "value": "", "isEven": true }, { "value": 3, "isEven": false },
+    { "value": "", "isEven": false }, { "value": "", "isEven": false }, { "value": 1, "isEven": false },
+    { "value": "", "isEven": false }, { "value": "", "isEven": true }, { "value": "", "isEven": true }
   ],
   [
-    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
-    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
-    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
+    { "value": "", "isEven": true }, { "value": 7, "isEven": false }, { "value": "", "isEven": false },
+    { "value": 1, "isEven": false }, { "value": "", "isEven": false }, { "value": "", "isEven": true },
+    { "value": "", "isEven": true }, { "value": "", "isEven": false }, { "value": "", "isEven": true },
   ],
   [
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
-    { "value": "", "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
-    { "value": "", "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": "", "isEven": false }, { "value": "", "isEven": true }, { "value": 8, "isEven": true },
+    { "value": "", "isEven": true }, { "value": "", "isEven": true }, { "value": "", "isEven": false },
+    { "value": 5, "isEven": false }, { "value": "", "isEven": false }, { "value": "", "isEven": false },
   ],
   [
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": true }, { "value": 1, "isEven": false },
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": true }, { "value": 1, "isEven": false },
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false }
+    { "value": "", "isEven": false }, { "value": "", "isEven": true }, { "value": "", "isEven": false },
+    { "value": "", "isEven": false }, { "value": "", "isEven": true }, { "value": 3, "isEven": false },
+    { "value": "", "isEven": true }, { "value": 4, "isEven": true }, { "value": "", "isEven": false }
   ],
   [
-    { "value": 2, "isEven": false }, { "value": "", "isEven": false }, { "value": 2, "isEven": false },
-    { "value": 2, "isEven": false }, { "value": "", "isEven": false }, { "value": 2, "isEven": false },
-    { "value": 2, "isEven": false }, { "value": "", "isEven": false }, { "value": 2, "isEven": false },
+    { "value": "", "isEven": true }, { "value": "", "isEven": false }, { "value": "", "isEven": false },
+    { "value": 8, "isEven": true }, { "value": "", "isEven": false }, { "value": "", "isEven": false },
+    { "value": 6, "isEven": true }, { "value": "", "isEven": false }, { "value": "", "isEven": true },
   ],
   [
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": "", "isEven": false }, { "value": "", "isEven": false }, { "value": "", "isEven": false },
+    { "value": "", "isEven": true }, { "value": 1, "isEven": false }, { "value": "", "isEven": true },
+    { "value": "", "isEven": true }, { "value": "", "isEven": true }, { "value": "", "isEven": false },
   ],
   [
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": true }, { "value": 1, "isEven": false },
-    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false }
+    { "value": 6, "isEven": true }, { "value": "", "isEven": true }, { "value": "", "isEven": true },
+    { "value": "", "isEven": true }, { "value": "", "isEven": false }, { "value": "", "isEven": false },
+    { "value": "", "isEven": false }, { "value": "", "isEven": false }, { "value": 7, "isEven": true }
   ]
 ]
 
