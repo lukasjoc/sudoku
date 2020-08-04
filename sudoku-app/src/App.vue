43412993a0e566f335800345a71eac4af57845e0
lukasjoc <jochamlu@gmail.com>
2020-08-04 17:10:35 +0200

    add props and use them for Board Component

--- sudoku-app/src/App.vue
+++ sudoku-app/src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <Board/>
-	</div>
+    <Board :data="datatest" />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Board from "./components/Board.vue";
+import defaultdata from "./data";
 
 export default Vue.extend({
   name: "App",
   components: {
-    Board
+    Board,
+  },
+  data: () => {
+    return {
+      datatest: defaultdata
+    }
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ sudoku-app/src/data.ts
@@ -0,0 +1,49 @@
+const defaultdata: object = [
+  [
+    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
+    { "value": 2, "isEven": false }, { "value": 4, "isEven": false }, { "value": 2, "isEven": false },
+    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 3, "isEven": false },
+  ],
+  [
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+  ],
+  [
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": true }, { "value": 1, "isEven": true }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false }
+  ],
+  [
+    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
+    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
+    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
+  ],
+  [
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+  ],
+  [
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": true }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false }
+  ],
+  [
+    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
+    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
+    { "value": 2, "isEven": false }, { "value": 2, "isEven": false }, { "value": 2, "isEven": false },
+  ],
+  [
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+  ],
+  [
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false },
+    { "value": 1, "isEven": false }, { "value": 1, "isEven": false }, { "value": 1, "isEven": false }
+  ]
+]
+
+export default defaultdata
