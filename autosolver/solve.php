046ebfa0927d77d5b3ce9507748a8104d2b5259b
lukasjoc <jochamlu@gmail.com>
2020-08-10 15:15:16 +0200

    add basic autosolver api

---
+++ autosolver/solve.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+/**
+ * Solver
+ *
+ */
+class Solver {
+
+	/**
+	 * Constructor
+	 *
+	 * @param string $puzzleString
+	 * @access public
+	 */
+	function __construct(string $puzzleString) {
+		$this->puzzleString = $puzzleString;
+	}
+
+	/**
+	 * solveOddEven
+	 *
+	 * @access public
+	 * @return Array
+	 */
+	function solveOddEven(): Array {
+		return [];
+	}
+
+}
+
+
